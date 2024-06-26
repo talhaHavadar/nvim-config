8dfe509084a1eef9eeb6c47e7031e696f5bbf153
Talha Can Havadar <talha.can.havadar@canonical.com>
2024-06-26 17:53:47 +0200

    add signature help functionality of lsp
    Signed-off-by: Talha Can Havadar <talha.can.havadar@canonical.com>

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,59 +1,59 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"taplo",
-					"tsserver",
-					"rust_analyzer",
-					"yamlls",
-				},
-			})
-		end,
-	},
-	{
-		"mrcjkb/rustaceanvim",
-		dependencies = { "neovim/nvim-lspconfig" },
-		version = "^4", -- Recommended
-		lazy = false, -- This plugin is already lazy
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = { "b0o/schemastore.nvim" },
-		config = function()
-			local lspconfig = require("lspconfig")
-			--local util = require("lspconfig/util")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.taplo.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.yamlls.setup({
-				capabilities = capabilities,
-				settings = {
-					yaml = {
-						schemaStore = {
-							enable = false,
-							url = "",
-						},
-						schemas = require("schemastore").yaml.schemas(),
-					},
-				},
-			})
-			--[[ rustaceanvim will setup lspconfig
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "taplo",
+                    "tsserver",
+                    "rust_analyzer",
+                    "yamlls",
+                },
+            })
+        end,
+    },
+    {
+        "mrcjkb/rustaceanvim",
+        dependencies = { "neovim/nvim-lspconfig" },
+        version = "^4", -- Recommended
+        lazy = false, -- This plugin is already lazy
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = { "b0o/schemastore.nvim" },
+        config = function()
+            local lspconfig = require("lspconfig")
+            --local util = require("lspconfig/util")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.taplo.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.yamlls.setup({
+                capabilities = capabilities,
+                settings = {
+                    yaml = {
+                        schemaStore = {
+                            enable = false,
+                            url = "",
+                        },
+                        schemas = require("schemastore").yaml.schemas(),
+                    },
+                },
+            })
+            --[[ rustaceanvim will setup lspconfig
             lspconfig.rust_analyzer.setup({
                 capabilities = capabilities,
                 filetypes = { 'rust' },
@@ -66,10 +66,17 @@ return {
                     }
                 }
             })]]
-			--
-			vim.keymap.set("n", "<leader>d", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            --
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(e)
+                    local opts = { silent = true, buffer = e.buf }
+
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set({ "i", "n" }, "<C-k>", vim.lsp.buf.signature_help, opts)
+                end,
+            })
+        end,
+    },
 }
--- lua/remap.lua
+++ lua/remap.lua
@@ -5,12 +5,12 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set('x', '<leader>p', '"_dP')
+vim.keymap.set("x", "<leader>p", '"_dP')
 
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent=true})
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
