a1052f8b20a37e89e22917446586adae7a52609b
Talha Can Havadar <havadartalha@gmail.com>
2024-06-28 22:06:03 +0200

    add cloak nvim and change filters for telescope
    Signed-off-by: Talha Can Havadar <havadartalha@gmail.com>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,26 +1,37 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.6",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set("n", "<C-s>", builtin.grep_string, {})
-        end,
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
-            require("telescope").load_extension("ui-select")
-        end,
-    },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = {
+						".git/*",
+						"node_modules",
+						"target",
+					},
+				},
+			})
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<C-p>", function()
+				builtin.find_files({ hidden = true, no_ignore = true })
+			end, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set({ "n", "i" }, "<C-f>", builtin.grep_string, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
