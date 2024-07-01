cd7d1893dcd4931119194ae39f5f816e71234eae
Talha Can Havadar <havadartalha@gmail.com>
2024-07-01 13:36:09 +0200

    remove unused plugins
    Signed-off-by: Talha Can Havadar <havadartalha@gmail.com>

--- lua/remap.lua
+++ lua/remap.lua
@@ -15,3 +15,8 @@ vim.keymap.set("i", "<C-r>", "<esc>:%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+vim.keymap.set("n", "<C-q>n", "<cmd>cnext<cr>")
+vim.keymap.set("n", "<C-q>p", "<cmd>cprev<cr>")
+vim.keymap.set("n", "<C-q>q", "<cmd>cquit<cr>")
+vim.keymap.set("n", "<C-b>", "<cmd>Ex<cr>")
